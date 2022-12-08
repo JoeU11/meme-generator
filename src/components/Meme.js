b93855ac496f31276c8fee60a0a21f5eca1ef8ae
joe <joe.ulica@gmail.com>
2022-12-08 17:22:08 -0500

    add hardcoded memes data

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,13 +1,16 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default function () {
+  function getMeme() {
+    console.log("hi")
+  }
   return (
     <section className="meme">
-      <form className="form">
+      <div className="form">
         <input type="text" className="form-input"
           placeholder="top text"></input>
         <input type="text" className="form-input" placeholder="bottom text"></input>
-        <button className="form-button">Get a new meme image 🖼</button>
-      </form>
+        <button className="form-button" onClick={getMeme}>Get a new meme image 🖼</button>
+      </div>
     </section>
   )
 }
